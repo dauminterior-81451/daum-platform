c93e05176cf854527ad46b77a837c83d2015e703
dauminterior-81451 <ddr076511@gmail.com>
2026-04-25 19:20:26 +0900

    fix: 고객 페이지 관리자 사이드바 충돌 수정

--- app/components/ConditionalSidebar.tsx
+++ app/components/ConditionalSidebar.tsx
@@ -6,5 +6,6 @@ import Sidebar from './Sidebar'
 export default function ConditionalSidebar() {
   const pathname = usePathname()
   if (pathname === '/login') return null
+  if (/^\/client(\/|$)/.test(pathname)) return null
   return <Sidebar />
 }
