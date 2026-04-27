ab32fdf1e632902e6fc6d8f555ddb6641f6603b0
dauminterior-81451 <ddr076511@gmail.com>
2026-04-27 16:45:21 +0900

    remove: 고객 페이지 카카오 공유 버튼 제거

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next'
-import Script from 'next/script'
 import './globals.css'
 import ConditionalSidebar from './components/ConditionalSidebar'
 
@@ -12,10 +11,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ko" className="h-full">
       <body className="h-full flex bg-gray-50 text-gray-900">
-        <Script
-          src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.4/kakao.min.js"
-          strategy="afterInteractive"
-        />
         <ConditionalSidebar />
         <main className="flex-1 overflow-auto pt-14 md:pt-0">{children}</main>
       </body>
